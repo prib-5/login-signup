7bec09cc73408c598ebb34151b0494dd90169076
prib-5 <128231938+prib-5@users.noreply.github.com>
2024-01-04 01:22:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from './logo.svg'
+import './App.css'
+import Loginsignup from "./loginsignup-components/loginsignup";
+
+function App() {
+  return (
+    <div>
+      <Loginsignup />
+    </div>
+  );
+  }
+
+export default App;
